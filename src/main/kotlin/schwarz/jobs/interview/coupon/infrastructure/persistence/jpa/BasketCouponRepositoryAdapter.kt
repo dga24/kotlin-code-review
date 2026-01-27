d0ebe7324c4ab04d2f75bced4e29383d068d7407
david <garciadavid.24@gmail.com>
2026-01-27 17:38:45 +0100

    improved

--- src/main/kotlin/schwarz/jobs/interview/coupon/infrastructure/persistence/jpa/BasketCouponRepositoryAdapter.kt
+++ src/main/kotlin/schwarz/jobs/interview/coupon/infrastructure/persistence/jpa/BasketCouponRepositoryAdapter.kt
@@ -1,6 +1,5 @@
 package schwarz.jobs.interview.coupon.infrastructure.persistence.jpa
 
-import jakarta.persistence.EntityManager
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import schwarz.jobs.interview.coupon.application.port.out.BasketCouponRepository
@@ -9,7 +8,6 @@ import java.math.BigDecimal
 @Repository
 class BasketCouponRepositoryAdapter(
     private val basketCouponRepositoryJpa: BasketCouponRepositoryJpa,
-    private val entityManager: EntityManager,
 ) : BasketCouponRepository {
 
     @Transactional
@@ -19,7 +17,5 @@ class BasketCouponRepositoryAdapter(
             couponCode = couponCode,
             discount = discount
         )
-        entityManager.flush()
-        entityManager.clear()
     }
 }
